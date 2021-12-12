461a2ef447e0ec3262575025d8131667f65d9de1
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2021-12-13 00:27:49 +0530

    Loading added

---
+++ components/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+    return (
+        <div className='flex justify-center items-center text-3xl h-screen w-screen'>
+            <img src="/rolling.svg" alt="Loading" />
+        </div>
+    )
+}
+
+export default loading
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
+import Router from 'next/router'
+import { useState } from 'react'
 import BaseTemplate from '../components/baseTemplate'
 import '../styles/globals.css'
-
+import Loading from '../components/loading';
 function MyApp({ Component, pageProps }) {
+  
+  const [loading, setloading] = useState(false)
+
+  Router.events.on("routeChangeStart", () => {
+    setloading(true)
+  })
+
+  Router.events.on("routeChangeComplete", () => {
+    setloading(false)
+  })
+
   return (
     <BaseTemplate>
-      <Component {...pageProps} />
+      {
+        loading? 
+        <Loading />
+        :
+        <Component {...pageProps} />
+      }
+      
     </BaseTemplate>
   )
 }
